be7bdba48fa02773ccd6ba399718b9f28d8fb821
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-07-08 14:58:18 -0300

    Renamed Folders

---
+++ Assets/Scripts/CELevel0.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CELevel0 : CodeEditorLevel {
+
+    public void EnabledChanged(float i)
+    {
+        int id = 0;
+        values[id] = i == 1f ? "true" : "false";
+    }
+}
