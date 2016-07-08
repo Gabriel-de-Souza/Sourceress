a20c97e06820b84d1245d7bafb4f9ca44fe9d1ee
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-07-08 11:40:03 -0300

    Main Game

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+    private GameObject target = null;
+
+	void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.tag.Equals("Player"))
+        {
+            if (target == null)
+            {
+                target = col.gameObject;
+                target.transform.SetParent(transform);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            if (target == col.gameObject)
+            {
+                target.transform.SetParent(null);
+                target = null;
+            }
+        }
+    }
+}
